9e4a00d9477db41859fe4cd35b3e32c2a79790c0
Kpri <122019858+ImKpri@users.noreply.github.com>
2024-05-15 21:00:26 +0200

    Update sv.lua

--- server/sv.lua
+++ server/sv.lua
@@ -84,18 +84,3 @@ AddEventHandler('esx_armour:addwhenmax', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.addInventoryItem('armor', 1)
 end)
-
-RegisterServerEvent('esx_armour:handcuffremove')
-AddEventHandler('esx_armour:handcuffremove', function()
-	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('handcuff', 1)
-end)
-
-
-ESX.RegisterUsableItem('handcuff', function(source)
-	TriggerClientEvent('esx_armour:handcuff', source)
-end)
-
-ESX.RegisterUsableItem('cutting_pliers', function(source)
-	TriggerClientEvent('esx_armour:cutting_pliers', source)
-end)
